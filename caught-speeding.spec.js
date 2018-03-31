1dd6de059e9fa80655484cf6bdb8cb6f5cabd319
Sean J. Gallen <sean.j.gallen@avanade.com>
2018-03-30 23:08:59 -0500

    added new tests

--- caught-speeding.spec.js
+++ caught-speeding.spec.js
@@ -1,25 +1,49 @@
 describe('caughtSpeeding', () => {
-    it('should return 0 if speed is 60 and is not birthday', () => {
-        const speed = 60;
-        const isBirthday = false;
-        const expected = 0;
-        const result = caughtSpeeding(speed, isBirthday);
-        expect(expected).toEqual(result);
-    });
+	it('should return 0 if speed is 60 and is not birthday', () => {
+		const speed = 60;
+		const isBirthday = false;
+		const expected = 0;
+		const result = caughtSpeeding(speed, isBirthday);
+		expect(expected).toEqual(result);
+	});
 
-    it('should return 1 if speed is 65 and is not birthday', () => {
-        const speed = 65;
-        const isBirthday = false;
-        const expected = 1;
-        const result = caughtSpeeding(speed, isBirthday);
-        expect(expected).toEqual(result);
-    });
+	it('should return 1 if speed is 65 and is not birthday', () => {
+		const speed = 65;
+		const isBirthday = false;
+		const expected = 1;
+		const result = caughtSpeeding(speed, isBirthday);
+		expect(expected).toEqual(result);
+	});
 
-    it('should return 0 if speed is 65 and is birthday', () => {
-        const speed = 65;
-        const isBirthday = true;
-        const expected = 0;
-        const result = caughtSpeeding(speed, isBirthday);
-        expect(expected).toEqual(result);
-    });
+	it('should return 0 if speed is 65 and is birthday', () => {
+		const speed = 65;
+		const isBirthday = true;
+		const expected = 0;
+		const result = caughtSpeeding(speed, isBirthday);
+		expect(expected).toEqual(result);
+	});
+
+	it('should return 1 if speed is over 60 but less than 80 and is not your birthday', () => {         
+		const speed = Math.floor(Math.random() * (80 - 61 + 1)) + 61;        
+		const isBirthday = false;         
+		const expected = 1;         
+		const result = caughtSpeeding(speed, isBirthday);         
+		expect(expected).toEqual(result);     
+	});
+
+	it('should return 2 if speed is over 80 and is not your birthday', () => { 		
+		const speed = Math.floor(Math.random() * 10 + 80); 		
+		const isBirthday = false; 		
+		const expected = 1; 		
+		const result = caughtSpeeding(speed, isBirthday); 		
+		expect(expected).toEqual(result); 
+	});	
+
+	it('should return 1 if speed is between 80 and 85 and it is your birthday'), () => {
+		const speed = Math.floor(Math.random() * (85 - 80 + 1)) + 80;
+		const isBirthday = true;
+		const expected = 1;
+		const result = caughtSpeeding(speed, isBirthday);
+		expect(expected).toEqual(result);
+	});
 })
